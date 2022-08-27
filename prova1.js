e9bb671df554c5fc93f264267573acc908b2e1c9
userflavio <112090618+userflavio@users.noreply.github.com>
2022-08-27 12:45:41 -0300

    Add files via upload

---
+++ prova1.js
@@ -0,0 +1,75 @@
+
+
+
+/*Receber duas notas de um aluno (não superior a 10, nem inferior a 0)
+e mostrar na tela a média aritimética dessas notas*/
+
+/*const meuInput = document.querySelector("#meuInput");
+meuInput.onclick = () => console.log("Clicou em mim");*/
+
+/*function media (v1v2){
+    return (v1+v2) / 2;
+}
+v3= media (55,1024);
+console.log(v3);*/
+
+
+/*
+function nome (lista de argumento){
+    código do método
+}
+*/
+
+/*Assinatura
+function exibirMenu(nome - "Não logado!!"){
+    if(nome != "Não logado!!||"){
+    console.log("..........................");
+    console.log("Bem vindo ao meu programa!");
+    console.log("Usuário logado: " + nome )
+    console.log("1 - Cadastrar Registro");
+    console.log("2 - Editar Registtro");
+    console.log("3 - Excluir Registro");
+    console.log("4 - Cadastrar Registro");
+    console.log("..........................");
+    }
+
+}
+*/
+
+
+/*let funcionario = Object();
+funcionario.nome = "Clemilton Souza";
+funcionario. idade = 33
+funcionario.email = "clemilton@c3c.com.br"
+console.table(funcionario);*/
+
+/*let pares =[0,2,4,6,8,10];
+pares[4] = 18
+console.log(pares);*/
+
+/*
+for(let i = 0; i < 100; i+=1){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+*/
+
+/*let i = 0;
+while(i<100){
+    if(i % 2 ==1){
+        console.log(i);
+    }
+    i+=1;
+}
+console.log("Finalizei");
+*/
+
+/*let contador = 0;
+do{
+    if(contador % 2 == 1){
+        console.log(contador);
+    }
+    contador+=1;
+}while(contador < 100);
+*/
